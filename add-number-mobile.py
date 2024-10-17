cdad642a9a9fa956f826a029be769ac6a8780b2d
radinettehad <radin_ettehad@yahoo.com>
2024-10-17 17:07:12 +0330

    creat new project for group 4

---
+++ add-number-mobile.py
@@ -0,0 +1,9 @@
+list1 = [9121234567, 935123456]
+list2 = []
+
+for i in list1:
+    if str(i)[0] != '0':
+        modified_number = ('0' + str(i)[0:])
+        list2.append(modified_number)
+
+print(list2)

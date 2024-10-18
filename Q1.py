0ea20984f3b24cbeb7239843ee6730cac54bf4c9
zohreh126 <zohrehpakseresht.3831@gmail.com>
2024-10-18 15:43:21 +0330

    new line

--- Q1.py
+++ Q1.py
@@ -1,8 +1,15 @@
-import os
+# import os
+#
+# remote_server = "google.com"
+#
+# if os.name == 'nt':
+#     os.system(f"ping -n 5 {remote_server}")
+# else:
+#     os.system(f"ping -c 5 {remote_server}")
 
-remote_server = "google.com"
-
-if os.name == 'nt':
-    os.system(f"ping -n 5 {remote_server}")
-else:
-    os.system(f"ping -c 5 {remote_server}")
+import subprocess
+def ping(server, cont=5):
+    command = ['ping', '-c', str(cont), server]
+    result = subprocess.run(command, shell=False, capture_output=True, text=True)
+    print(result.stdout)
+ping("google.com")
